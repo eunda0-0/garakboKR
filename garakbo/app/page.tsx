506e3f9eb45cba23ed16fd26992eb2ef480fddaf
eunda0-0 <eunda0130@gmail.com>
2026-04-06 10:51:42 +0900

    Update page.tsx

--- garakbo/app/page.tsx
+++ garakbo/app/page.tsx
@@ -1,19 +1,9 @@
-import AdBanner from '@/components/AdBanner'
 import GarakboApp from '@/components/GarakboApp'
 
 export default function Home() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100vh', overflow: 'hidden' }}>
-      {/* 상단 광고 */}
-      <AdBanner position="top" />
-
-      {/* 가락보 앱 */}
-      <div style={{ flex: 1, display: 'flex', flexDirection: 'column', overflow: 'hidden' }}>
-        <GarakboApp />
-      </div>
-
-      {/* 하단 광고 */}
-      <AdBanner position="bottom" />
+      <GarakboApp />
     </div>
   )
 }
